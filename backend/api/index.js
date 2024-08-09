9a45fddbd370304e62bd2f466577769e272ccca4
fitri <sitifitriani8910@gmail.com>
2024-08-09 12:11:11 +0700

    second commit

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,27 +2,73 @@ import express from "express";
 import { connection } from "../database.js";
 
 const app = express();
-
 app.use(express.json());
 
+//============= Cara 1 menggunakan Database mariaDB =====================
+// // get all mahasiswa
+// app.get("/mahasiswa", async(_req,res)=>{
+//     const result = await connection.query("SELECT * FROM mahasiswa");
+//     res.json(result);
+// })
 
+// // Add mahasiswa
+// app.post("/api/mahasiswa", async (req,res)=>{
+//     await connection.query(
+//         "INSERT INTO mahasiswa (id,nama,jurusan) VALUES (?,?,?)",
+//         [req.body.id,req.body.nama,req.body.jurusan]
+//     )
+//     res.send("mahasiswa berhasil di tambahkan");
+// })
+
+// // edit mahasiswa
+// app.put("/api/mahasiswa/:id", async (req,res)=>{
+//     await connection.query
+//     ("UPDATE mahasiswa SET nama =?, jurusan = ? WHERE id=?",
+//         [req.body.nama, req.body.jurusan, req.params.id]);
+//     res.send("mahasiswa berhasil di edit");
+// });
+
+// // delete mahasiswa
+// app.delete("/api/mahasiswa/:id", async (req,res)=>{
+//     await connection.query("DELETE FROM mahasiswa WHERE id=?",
+//         [req.params.id]);
+//         res.send("mahasiswa berhasil di hapus");
+// })
+
+
+//============= Cara 2 menggunakan Database postgres =====================
 // get all mahasiswa
 app.get("/mahasiswa", async(_req,res)=>{
     const result = await connection.query("SELECT * FROM mahasiswa");
     res.json(result);
 })
 
-
 // Add mahasiswa
 app.post("/api/mahasiswa", async (req,res)=>{
-    await connection.execute(
-        "INSERT INTO mahasiswa (id,nama,jurusan) VALUES (?,?,?)",
+    await connection.query(
+        "INSERT INTO mahasiswa (id,nama,jurusan) VALUES ($1,$2,$3)",
         [req.body.id,req.body.nama,req.body.jurusan]
     )
     res.send("mahasiswa berhasil di tambahkan");
 })
 
-    app.listen(3000,()=>{
+// edit mahasiswa
+app.put("/api/mahasiswa/:id", async (req,res)=>{
+    await connection.query
+    ("UPDATE mahasiswa SET nama =$1, jurusan = $2 WHERE id=$3",
+        [req.body.nama, req.body.jurusan, req.params.id]);
+    res.send("mahasiswa berhasil di edit");
+});
+
+// delete mahasiswa
+app.delete("/api/mahasiswa/:id", async (req,res)=>{
+    await connection.query("DELETE FROM mahasiswa WHERE id=$1",
+        [req.params.id]);
+        res.send("mahasiswa berhasil di hapus");
+})
+
+
+app.listen(3000,()=>{
         console.log("Server telah berjalan di port 3000");
     })
     
--- backend/database.js
+++ backend/database.js
@@ -1,13 +1,27 @@
-import mariadb from "mariadb"
+//============= Cara 1 menggunakan Database mariaDB =====================
+// import mariadb from "mariadb"
 import dotenv from "dotenv"
 dotenv.config()
 
-export const connection = await mariadb.createConnection({
+// export const connection = await mariadb.createConnection({
+//     host:process.env.DB_HOST,
+//     port:process.env.DB_PORT,
+//     database:process.env.DB_NAME,
+//     user:process.env.DB_USER,
+//     password:process.env.DB_PASSWORD 
+// })
+
+
+//============= Cara 2 menggunakan Database postgres =====================
+import pkg from "pg";
+ const {Pool} = pkg;
+
+ export const connection = new Pool({
     host:process.env.DB_HOST,
     port:process.env.DB_PORT,
     database:process.env.DB_NAME,
     user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD 
-})
+    password:process.env.DB_PASSWORD
+ });
 
-
+connection.connect().than(()=>console.log("successfully connected to the database"))
